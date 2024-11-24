264e761eb110d8490fe06aac3988b3a4175a3336
syed abdul muqeeth <syed_abdul_muqeeth@proton.me>
2024-11-24 16:16:27 +0530

    navbar working except cart

--- app/api/addtocart/route.ts
+++ app/api/addtocart/route.ts
@@ -1,3 +1,4 @@
+
 import { NextRequest, NextResponse } from 'next/server';
 const CoCartAPI = require("@cocart/cocart-rest-api").default;
 
--- app/component/navbar.tsx
+++ app/component/navbar.tsx
@@ -9,128 +9,128 @@ import {
   NavbarMenu,
   NavbarMenuItem,
 } from "@nextui-org/navbar";
-import { useRouter } from "next/navigation"
-
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import NextImage from "next/image";
-import { Search, ShoppingBag, Heart, Truck } from "lucide-react"; // Assuming you're using lucide-react for icons
+import { Search, ShoppingBag, Heart, Truck } from "lucide-react";
 import SiteLogo from "../../public/sitelogo.jpeg";
-import {Badge} from "@nextui-org/badge";
+import { Badge } from "@nextui-org/badge";
 
+const cart_count = 0;
 
 export default function Nav_bar() {
-
   const router = useRouter();
-
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
   const handleSearchClick = () => {
-    router.push('/search')
+    router.push('/search');
   };
 
   const handleTrackOrderClick = () => {
-    router.push('/trackorder')
+    router.push('/trackorder');
+  };
+
+  const handleCartClick = () => {
+    router.push('/cart');
   };
 
   const handleLogoClick = () => {
-    router.push('/')
+    router.push('/');
   };
 
   const menuItems = [
-    "Shop",
-    "Search",
-    "Cart",
-    "Wishlist",
-    "Contact Us",
-    "FAQ",
-    "Refund & Returns",
-    "Track Order",
-    "Privacy Policy",
-    "Terms & Condition",
+    { label: "Store", route: "/store" },
+    { label: "Search", route: "/search" },
+    { label: "Cart", route: "/cart" },
+    { label: "Contact Us", route: "/contact" },
+    { label: "FAQ", route: "/faq" },
+    { label: "Refund & Returns", route: "/refund" },
+    { label: "Track Order", route: "/trackorder" },
+    { label: "Privacy Policy", route: "/privacy" },
+    { label: "Terms & Condition", route: "/terms" },
   ];
+
+  const handleMenuItemClick = (route:any) => {
+    router.push(route);
+    setIsMenuOpen(false); // Close the menu after clicking
+  };
 
   return (
     <>
-    <div className="bg-white w-full text-small text-black p-3 flex items-center justify-center">
-      FREE EXPRESS SHIPPING OVER $100 USD
-    </div>
-    <Navbar className="bg-black text-white" onMenuOpenChange={setIsMenuOpen}>
-      <NavbarContent>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden"
-        />
-        <NavbarBrand>
-          <NextImage
-            onClick={handleLogoClick}
-            src={SiteLogo}
-            alt="Site Logo"
-            width={120}
-            height={40}
-            priority
+      <div className="bg-white w-full text-small text-black p-3 flex items-center justify-center">
+        FREE EXPRESS SHIPPING OVER $100 USD
+      </div>
+      <Navbar className="bg-black text-white" onMenuOpenChange={setIsMenuOpen}>
+        <NavbarContent>
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+            className="sm:hidden"
           />
-        </NavbarBrand>
-      </NavbarContent>
+          <NavbarBrand>
+            <NextImage
+              onClick={handleLogoClick}
+              src={SiteLogo}
+              alt="Site Logo"
+              width={120}
+              height={40}
+              priority
+              className="cursor-pointer"
+            />
+          </NavbarBrand>
+        </NavbarContent>
 
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem className="text-white hover:font-bold">
-          <Link color="foreground" href="#">
-            Men&apos;s
-          </Link>
-        </NavbarItem>
-        <NavbarItem className="text-white hover:font-bold">
-          <Link href="#" aria-current="page">
-            Women&apos;s
-          </Link>
-        </NavbarItem>
-        <NavbarItem className="text-white hover:font-bold">
-          <Link color="foreground" href="#">
-            Accessories
-          </Link>
-        </NavbarItem>
-        <NavbarItem className="text-white hover:font-bold">
-          <Link color="foreground" href="#">
-            Kid&apos;s
-          </Link>
-        </NavbarItem>
-        <NavbarItem className="text-white hover:font-bold">
-          <Link color="foreground" href="#">
-            FootWare
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarContent justify="end" className="gap-4">
-        <Search onClick={handleSearchClick} className="h-5 cursor-pointer" />
-        <Badge content="99" className="border-none" shape="circle" color="danger">
-         <ShoppingBag className="h-5 cursor-pointer" />
-        </Badge>
-        <Badge content="22" className="border-none" shape="circle" color="danger">
-         <Heart className="h-5 cursor-pointer" />
-        </Badge>
-        <Truck onClick={handleTrackOrderClick} className="h-5 cursor-pointer" />
-      </NavbarContent>
-
-      <NavbarMenu className="bg-black text-white">
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem className="bg-black text-white" key={`${item}-${index}`}>
-            <Link
-              color={
-                index === 2
-                  ? "primary"
-                  : index === menuItems.length - 1
-                  ? "danger"
-                  : "foreground"
-              }
-              className="w-full bg-black text-white"
-              href="#"
-            >
-              {item}
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          <NavbarItem className="text-white hover:font-bold">
+            <Link color="foreground" href="#">
+              Men&apos;s
             </Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
-    </Navbar>
+          </NavbarItem>
+          <NavbarItem className="text-white hover:font-bold">
+            <Link href="#" aria-current="page">
+              Women&apos;s
+            </Link>
+          </NavbarItem>
+          <NavbarItem className="text-white hover:font-bold">
+            <Link color="foreground" href="#">
+              Accessories
+            </Link>
+          </NavbarItem>
+          <NavbarItem className="text-white hover:font-bold">
+            <Link color="foreground" href="#">
+              Kid&apos;s
+            </Link>
+          </NavbarItem>
+          <NavbarItem className="text-white hover:font-bold">
+            <Link color="foreground" href="#">
+              FootWare
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+
+        <NavbarContent justify="end" className="gap-4">
+          <Badge onClick={handleCartClick} content={cart_count} className="border-none" shape="circle" color="danger">
+            <ShoppingBag onClick={handleCartClick} className="h-5 cursor-pointer" />
+          </Badge>
+          <Search onClick={handleSearchClick} className="h-5 cursor-pointer" />
+          <Truck onClick={handleTrackOrderClick} className="h-5 cursor-pointer" />
+        </NavbarContent>
+
+        <NavbarMenu className="bg-black text-white">
+          {menuItems.map((item, index) => (
+            <NavbarMenuItem 
+              key={`${item.label}-${index}`}
+              className="bg-black text-white hover:bg-gray-800"
+            >
+              <button
+                onClick={() => handleMenuItemClick(item.route)}
+                className="w-full text-left py-2 px-4 text-white hover:text-gray-300"
+              >
+                {item.label}
+              </button>
+            </NavbarMenuItem>
+          ))}
+        </NavbarMenu>
+      </Navbar>
     </>
   );
 }
