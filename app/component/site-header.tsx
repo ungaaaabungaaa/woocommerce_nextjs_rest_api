ce652c21ddc32b2bdba791a20a984cad1a6fd413
syed abdul muqeeth <syed_abdul_muqeeth@proton.me>
2024-12-14 04:23:41 +0530

    fixed alignments

--- app/component/site-header.tsx
+++ app/component/site-header.tsx
@@ -3,7 +3,7 @@ export function SiteHeader() {
   return (
     <header className="w-full bg-white text-black dark:bg-black dark:text-white ">
       <div className="container flex  items-center justify-center">
-          <span className="text-sm ">SALE SALE SALE SALE SALE</span>
+          <span className="text-sm ">FREE EXPRESS SHIPPING OVER $100 USD</span>
       </div>
     </header>
   )
