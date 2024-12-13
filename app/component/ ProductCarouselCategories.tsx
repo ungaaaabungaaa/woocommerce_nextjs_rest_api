5ed65d771b220c67ffb5784a48de2c7e288e405c
syed abdul muqeeth <syed_abdul_muqeeth@proton.me>
2024-12-14 04:35:59 +0530

    fixed space issues

--- app/component/ ProductCarouselCategories.tsx
+++ app/component/ ProductCarouselCategories.tsx
@@ -133,13 +133,13 @@ const ProductCard = ({ product }: ProductCardProps) => {
             </div>
           )}
         </div>
-        <p className="text-white dark:text-black text-left text-balance text-base md:text-xl lg:text-2xl font-semibold tracking-[-0.015em] mb-2">
+        <p className="text-white dark:text-black text-left text-balance text-base md:text-xl lg:text-2xl font-semibold tracking-[-0.015em] mt-2">
           {product.title}
         </p>
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center mt-2">
           {product.sale_price && product.regular_price ? (
             <div className="flex items-center">
-              <span className="text-gray-500 dark:text-gray-400 text-1xl line-through">
+              <span className="text-gray-500 dark:text-gray-400 text-1xl line-through mr-2">
                 ${product.regular_price}
               </span>
               <span className="text-white dark:text-black font-bold text-1xl">
@@ -168,7 +168,7 @@ const ProductCard = ({ product }: ProductCardProps) => {
           </Button>
         </div>
 
-        <p className="max-w-[26rem] text-left text-base/6 text-neutral-200">
+        <p className="max-w-[26rem] text-left text-base/6 text-white dark:text-black mt-2">
           {product.description}
         </p>
         
