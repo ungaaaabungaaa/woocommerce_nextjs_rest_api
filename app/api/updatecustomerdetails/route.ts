081fb31b1355251a875df7fcd10ef80ddc2b66f9
syed <syed@kemuri.in>
2025-01-04 15:40:20 +0530

    fixed ts error for vercel route

--- app/api/updatecustomerdetails/route.ts
+++ app/api/updatecustomerdetails/route.ts
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
 
-export const api = new WooCommerceRestApi({
-  url: process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL,
-  consumerKey: process.env.WC_CONSUMER_KEY,
-  consumerSecret: process.env.WC_CONSUMER_SECRET,
+const api = new WooCommerceRestApi({
+  url: process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL!,
+  consumerKey: process.env.WC_CONSUMER_KEY!,
+  consumerSecret: process.env.WC_CONSUMER_SECRET!,
   version: "wc/v3",
   queryStringAuth: true,
 });
