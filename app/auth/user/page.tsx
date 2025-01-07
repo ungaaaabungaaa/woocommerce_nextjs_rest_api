c01d27ef23ed6696559be9930f6206430458bee2
syed <syed@kemuri.in>
2025-01-07 19:27:54 +0530

    adding user screen orderview , restet password

---
+++ app/auth/user/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// show all the orders
+// show all the reset password thing here
+// show the account edit options as well here
+
+function page() {
+  return <div></div>;
+}
+
+export default page;
