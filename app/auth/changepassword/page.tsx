6ef4191a12393f5cb77e48a5ce22144534da8750
syed <syed@kemuri.in>
2025-01-07 19:54:29 +0530

    change password functionality

---
+++ app/auth/changepassword/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <h1>Change Password For the Email logged in users only </h1>
+    </div>
+  );
+}
+
+export default page;
