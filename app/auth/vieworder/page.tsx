eec2b68f5af4866d2b194d5e2e618de1dcbe532c
syed <syed@kemuri.in>
2025-01-17 14:32:26 +0530

    fixing vercel issues

--- app/auth/vieworder/page.tsx
+++ app/auth/vieworder/page.tsx
@@ -183,12 +183,13 @@ export default function ViewOrder() {
           </Button>
         </div>
         <div className="flex items-center pt-4">
-          <p
+          <Button
             onClick={handleGoBack}
-            className=" h-4 text-sm text-gray-500 dark:text-gray-400  mt-2 cursor-pointer"
+            className=" h-4 text-sm text-gray-500 dark:text-gray-400  mt-2 cursor-pointer bg-transparent"
           >
+            {" "}
             &lt; &nbsp; <span className="underline">Order History</span>
-          </p>
+          </Button>
         </div>
         <OrderTracking order={mockOrder} />
       </div>
