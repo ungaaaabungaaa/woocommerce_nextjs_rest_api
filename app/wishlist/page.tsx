435ba00a989a5fb0c2b52fffeace6e0826e34ed2
syed <syed@kemuri.in>
2025-01-19 19:58:43 +0530

    added the route things

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -1,33 +1,37 @@
 "use client";
+
 import React, { useEffect, useState } from "react";
 import {
-  storeWishlist,
   showWishlist,
   removeWishlistItem,
   clearWishlist,
-  checkWishlist,
   wishlistCount,
 } from "@/helper/wishlistHelper";
 
-function page() {
-  const [count, setCount] = useState<number>(0); // Wishlist count
+export default function WishlistPage() {
+  const [count, setCount] = useState<number>(0);
   const [wishlist, setWishlist] = useState<number[]>([]);
 
   useEffect(() => {
-    setCount(wishlistCount());
-    setWishlist(showWishlist());
+    const fetchWishlist = async () => {
+      const wishlistItems = await showWishlist();
+      setWishlist(wishlistItems);
+      setCount(wishlistCount());
+    };
+
+    fetchWishlist();
   }, []);
 
   return (
     <div className="w-full flex align-middle justify-center items-center bg-black text-white dark:bg-white dark:text-black">
-      <div className=" bg-black text-white dark:bg-white dark:text-black w-full max-w-7xl">
+      <div className="w-full max-w-7xl">
         <main className="px-4 py-8">
-          <div>
-            <h1 className="text-3xl font-bold text-white dark:text-black">
-              WishList <span className="text-sm">{count} products</span>
+          <div className="flex justify-between items-center mb-6">
+            <h1 className="text-3xl font-bold">
+              Wishlist <span className="text-sm">({count} products)</span>
             </h1>
           </div>
-          <div className="bg-black text-white dark:bg-white dark:text-black w-full max-w-7xl">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             <ul>
               {wishlist.map((item) => (
                 <li key={item}>{item}</li>
@@ -39,10 +43,3 @@ function page() {
     </div>
   );
 }
-
-export default page;
-
-// show the wishlist items
-// make the api call
-// get the data
-// push it into the product cards
