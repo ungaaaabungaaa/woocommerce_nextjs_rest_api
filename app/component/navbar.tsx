1aa313fa4e7e23eac0661dbd3c9e7022c55bbadd
syed <syed@kemuri.in>
2025-02-05 17:28:29 +0530

    trying something

--- app/component/navbar.tsx
+++ app/component/navbar.tsx
@@ -143,242 +143,241 @@ export default function Nav_bar() {
 
   return (
     <>
-    <div className="w-full py-2 flex align-middle items-center justify-center bg-white text-black dark:bg-black dark:text-white">
-          <Navbar
-            maxWidth="xl"
-            className="bg-white text-black dark:bg-black dark:text-white h-8 w-full  max-w-7xl "
-          >
-            <NavbarContent className="sm:flex gap-4" justify="start">
-              <NavbarBrand className="pr-4 hidden sm:flex">
-                <div className="flex items-center gap-2">
-                  <ChevronLeft className="h-3 w-3" />
-                  <span className="text-xs">theclothvillagestore.com</span>
-                </div>
-              </NavbarBrand>
-            </NavbarContent>
+      <div className="w-full py-2 flex align-middle items-center justify-center bg-white text-black dark:bg-black dark:text-white">
+        <Navbar
+          maxWidth="xl"
+          className="bg-white text-black dark:bg-black dark:text-white h-8 w-full  max-w-7xl "
+        >
+          <NavbarContent className="sm:flex gap-4" justify="start">
+            <NavbarBrand className="pr-4 hidden sm:flex">
+              <div className="flex items-center gap-2">
+                <ChevronLeft className="h-3 w-3" />
+                <span className="text-xs">theclothvillagestore.com</span>
+              </div>
+            </NavbarBrand>
+          </NavbarContent>
 
-            <NavbarContent
-              className="flex absolute left-1/2 transform -translate-x-1/2"
-              justify="center"
+          <NavbarContent
+            className="flex absolute left-1/2 transform -translate-x-1/2"
+            justify="center"
+          >
+            <p className="text-xs font-medium">Official Cloth Village Store</p>
+          </NavbarContent>
+
+          <NavbarContent justify="end" className="gap-4">
+            <nav className="hidden gap-4 md:flex">
+              <Link
+                href="/contact"
+                className="text-xs text-muted-foreground hover:font-bold cursor-pointer"
+              >
+                Help
+              </Link>
+              <Link
+                href="/trackorder"
+                className="text-xs text-muted-foreground hover:font-bold cursor-pointer"
+              >
+                Orders & Returns
+              </Link>
+            </nav>
+          </NavbarContent>
+        </Navbar>
+      </div>
+      <div className="sticky top-0 z-50">
+        <div className="relative">
+          <div className="w-full py-3 flex align-middle items-center justify-center  text-white  dark:text-black border-b border-gray-700 dark:border-gray-200 bg-black dark:bg-white">
+            <Navbar
+              maxWidth="full"
+              className="text-white dark:text-black  bg-black dark:bg-white max-w-7xl "
+              onMenuOpenChange={setIsMenuOpen}
             >
-              <p className="text-xs font-medium">
-                Official Cloth Village Store
-              </p>
-            </NavbarContent>
-
-            <NavbarContent justify="end" className="gap-4">
-              <nav className="hidden gap-4 md:flex">
-                <Link
-                  href="/contact"
-                  className="text-xs text-muted-foreground hover:font-bold cursor-pointer"
-                >
-                  Help
-                </Link>
-                <Link
-                  href="/trackorder"
-                  className="text-xs text-muted-foreground hover:font-bold cursor-pointer"
-                >
-                  Orders & Returns
-                </Link>
-              </nav>
-            </NavbarContent>
-          </Navbar>
-        </div>
-    <div className="sticky top-0 z-50">
-      <div className="relative">
-      
-
-        <div className="w-full py-3 flex align-middle items-center justify-center  text-white  dark:text-black border-b border-gray-700 dark:border-gray-200 bg-black dark:bg-white">
-          <Navbar
-            maxWidth="full"
-            className="text-white dark:text-black  bg-black dark:bg-white max-w-7xl "
-            onMenuOpenChange={setIsMenuOpen}
-          >
-            <NavbarContent className="sm:flex gap-4 bg-black dark:bg-white" justify="start">
-              <NavbarMenuToggle
-                aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-                className="sm:hidden"
-              />
-              <NavbarBrand className="pr-2">
-                <NextImage
-                  onClick={handleLogoClick}
-                  src={Sitelogo}
-                  alt="Site Logo"
-                  width={280}
-                  height={200}
-                  priority
-                  className="cursor-pointer"
+              <NavbarContent
+                className="sm:flex gap-4 bg-black dark:bg-white"
+                justify="start"
+              >
+                <NavbarMenuToggle
+                  aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+                  className="sm:hidden"
                 />
-              </NavbarBrand>
+                <NavbarBrand className="pr-2">
+                  <NextImage
+                    onClick={handleLogoClick}
+                    src={Sitelogo}
+                    alt="Site Logo"
+                    width={180}
+                    height={100}
+                    priority
+                    className="cursor-pointer"
+                  />
+                </NavbarBrand>
 
-              <NavbarContent className="hidden sm:flex gap-8" justify="start">
-                {megaMenus.map((menu) => (
-                  <NavbarItem
-                    key={menu.name}
-                    className="text-white hover:font-bold dark:text-black"
-                    onMouseEnter={() =>
-                      supportsHover && setVisibleMegaMenu(menu.name)
-                    }
-                    // onMouseLeave={() => supportsHover && setVisibleMegaMenu("")}
-                  >
-                    <Link href="#">{menu.name}&apos;s</Link>
-                  </NavbarItem>
-                ))}
+                <NavbarContent className="hidden sm:flex gap-8" justify="start">
+                  {megaMenus.map((menu) => (
+                    <NavbarItem
+                      key={menu.name}
+                      className="text-white hover:font-bold dark:text-black"
+                      onMouseEnter={() =>
+                        supportsHover && setVisibleMegaMenu(menu.name)
+                      }
+                      // onMouseLeave={() => supportsHover && setVisibleMegaMenu("")}
+                    >
+                      <Link href="#">{menu.name}&apos;s</Link>
+                    </NavbarItem>
+                  ))}
+                </NavbarContent>
               </NavbarContent>
-            </NavbarContent>
 
-            <NavbarContent justify="end" className="gap-4">
-              <Search
-                onClick={handleSearchClick}
-                className="h-8 cursor-pointer text-white dark:text-black"
-              />
+              <NavbarContent justify="end" className="gap-4">
+                <Search
+                  onClick={handleSearchClick}
+                  className="h-8 cursor-pointer text-white dark:text-black"
+                />
 
-              {isAuthenticated ? (
-                <div
-                  className={supportsHover ? "hover-enabled" : ""}
-                  onMouseEnter={() => handleIconMouseEnter("account")}
-                  onMouseLeave={handleIconMouseLeave}
-                >
-                  <UserCircle
-                    className="h-8 cursor-pointer text-white dark:text-black"
-                    onClick={() => router.push("/auth/user")}
-                  />
-                </div>
-              ) : (
-                <div
-                  className={supportsHover ? "hover-enabled" : ""}
-                  onMouseEnter={() => handleIconMouseEnter("account")}
-                  onMouseLeave={handleIconMouseLeave}
-                >
-                  <User
-                    className="h-8 cursor-pointer text-white dark:text-black"
-                    onClick={() => router.push("/auth/register")}
-                  />
-                </div>
-              )}
-
-              <Badge
-                content={wishlistCount}
-                className="border-none"
-                shape="circle"
-                color="danger"
-              >
-                <div
-                  className={supportsHover ? "hover-enabled" : ""}
-                  onMouseEnter={() => handleIconMouseEnter("wishlist")}
-                  onMouseLeave={handleIconMouseLeave}
-                >
-                  <Heart
-                    onClick={handleWishlistClick}
-                    className="h-8 cursor-pointer text-white dark:text-black"
-                  />
-                </div>
-              </Badge>
-
-              <Badge
-                content={cartCount}
-                className="border-none"
-                shape="circle"
-                color="danger"
-              >
-                <div
-                  className={supportsHover ? "hover-enabled" : ""}
-                  onMouseEnter={() => handleIconMouseEnter("cart")}
-                  onMouseLeave={handleIconMouseLeave}
-                >
-                  <ShoppingBag
-                    onClick={handleCartClick}
-                    className="h-8 cursor-pointer text-white dark:text-black"
-                  />
-                </div>
-              </Badge>
-
-              {mounted && (
-                <button onClick={toggleTheme} className="focus:outline-none">
-                  {theme === "dark" ? (
-                    <Sun className="h-8 cursor-pointer text-white dark:text-black" />
-                  ) : (
-                    <Moon className="h-8 cursor-pointer text-white dark:text-black" />
-                  )}
-                </button>
-              )}
-            </NavbarContent>
-
-            <NavbarMenu className=" text-white dark:text-white debug">
-              {menuItems.map((item, index) => (
-                <NavbarMenuItem
-                  key={`${item.label}-${index}`}
-                  className=" text-white  dark:text-black "
-                >
-                  <button
-                    onClick={() => handleMenuItemClick(item.route)}
-                    className="w-full text-left py-2 px-4 text-white hover:text-gray-300 dark:text-black"
+                {isAuthenticated ? (
+                  <div
+                    className={supportsHover ? "hover-enabled" : ""}
+                    onMouseEnter={() => handleIconMouseEnter("account")}
+                    onMouseLeave={handleIconMouseLeave}
                   >
-                    {item.label}
-                  </button>
-                </NavbarMenuItem>
-              ))}
-            </NavbarMenu>
-          </Navbar>
-        </div>
+                    <UserCircle
+                      className="h-8 cursor-pointer text-white dark:text-black"
+                      onClick={() => router.push("/auth/user")}
+                    />
+                  </div>
+                ) : (
+                  <div
+                    className={supportsHover ? "hover-enabled" : ""}
+                    onMouseEnter={() => handleIconMouseEnter("account")}
+                    onMouseLeave={handleIconMouseLeave}
+                  >
+                    <User
+                      className="h-8 cursor-pointer text-white dark:text-black"
+                      onClick={() => router.push("/auth/register")}
+                    />
+                  </div>
+                )}
 
-        {/* Mega Menus Container */}
-        <div className="absolute left-0 w-full">
-          {megaMenus.map((menu) => (
+                <Badge
+                  content={wishlistCount}
+                  className="border-none"
+                  shape="circle"
+                  color="danger"
+                >
+                  <div
+                    className={supportsHover ? "hover-enabled" : ""}
+                    onMouseEnter={() => handleIconMouseEnter("wishlist")}
+                    onMouseLeave={handleIconMouseLeave}
+                  >
+                    <Heart
+                      onClick={handleWishlistClick}
+                      className="h-8 cursor-pointer text-white dark:text-black"
+                    />
+                  </div>
+                </Badge>
+
+                <Badge
+                  content={cartCount}
+                  className="border-none"
+                  shape="circle"
+                  color="danger"
+                >
+                  <div
+                    className={supportsHover ? "hover-enabled" : ""}
+                    onMouseEnter={() => handleIconMouseEnter("cart")}
+                    onMouseLeave={handleIconMouseLeave}
+                  >
+                    <ShoppingBag
+                      onClick={handleCartClick}
+                      className="h-8 cursor-pointer text-white dark:text-black"
+                    />
+                  </div>
+                </Badge>
+
+                {mounted && (
+                  <button onClick={toggleTheme} className="focus:outline-none">
+                    {theme === "dark" ? (
+                      <Sun className="h-8 cursor-pointer text-white dark:text-black" />
+                    ) : (
+                      <Moon className="h-8 cursor-pointer text-white dark:text-black" />
+                    )}
+                  </button>
+                )}
+              </NavbarContent>
+
+              <NavbarMenu className=" text-white dark:text-white debug">
+                {menuItems.map((item, index) => (
+                  <NavbarMenuItem
+                    key={`${item.label}-${index}`}
+                    className=" text-white  dark:text-black "
+                  >
+                    <button
+                      onClick={() => handleMenuItemClick(item.route)}
+                      className="w-full text-left py-2 px-4 text-white hover:text-gray-300 dark:text-black"
+                    >
+                      {item.label}
+                    </button>
+                  </NavbarMenuItem>
+                ))}
+              </NavbarMenu>
+            </Navbar>
+          </div>
+
+          {/* Mega Menus Container */}
+          <div className="absolute left-0 w-full">
+            {megaMenus.map((menu) => (
+              <div
+                key={menu.name}
+                className={`absolute top-0 left-0 w-full transition-all duration-300 ${
+                  visibleMegaMenu === menu.name
+                    ? "opacity-100 visible"
+                    : "opacity-0 invisible pointer-events-none"
+                }`}
+                onMouseEnter={() =>
+                  supportsHover && setVisibleMegaMenu(menu.name)
+                }
+                onMouseLeave={() => supportsHover && setVisibleMegaMenu("")}
+              >
+                <menu.component />
+              </div>
+            ))}
+          </div>
+          {/* Icon Popups Container */}
+          <div className="absolute left-0 w-full">
             <div
-              key={menu.name}
-              className={`absolute top-0 left-0 w-full transition-all duration-300 ${
-                visibleMegaMenu === menu.name
+              className={`absolute top-full left-0 w-full transition-all duration-300 ${
+                visibleIconPopup === "cart"
                   ? "opacity-100 visible"
                   : "opacity-0 invisible pointer-events-none"
               }`}
-              onMouseEnter={() =>
-                supportsHover && setVisibleMegaMenu(menu.name)
-              }
-              onMouseLeave={() => supportsHover && setVisibleMegaMenu("")}
+              onMouseEnter={() => handleIconMouseEnter("cart")}
+              onMouseLeave={handleIconMouseLeave}
             >
-              <menu.component />
+              <CartPopUp />
             </div>
-          ))}
-        </div>
-        {/* Icon Popups Container */}
-        <div className="absolute left-0 w-full">
-          <div
-            className={`absolute top-full left-0 w-full transition-all duration-300 ${
-              visibleIconPopup === "cart"
-                ? "opacity-100 visible"
-                : "opacity-0 invisible pointer-events-none"
-            }`}
-            onMouseEnter={() => handleIconMouseEnter("cart")}
-            onMouseLeave={handleIconMouseLeave}
-          >
-            <CartPopUp />
-          </div>
-          <div
-            className={`absolute top-full left-0 w-full transition-all duration-300 ${
-              visibleIconPopup === "account"
-                ? "opacity-100 visible"
-                : "opacity-0 invisible pointer-events-none"
-            }`}
-            onMouseEnter={() => handleIconMouseEnter("account")}
-            onMouseLeave={handleIconMouseLeave}
-          >
-            <AccountPopUp />
-          </div>
-          <div
-            className={`absolute top-full left-0 w-full transition-all duration-300 ${
-              visibleIconPopup === "wishlist"
-                ? "opacity-100 visible"
-                : "opacity-0 invisible pointer-events-none"
-            }`}
-            onMouseEnter={() => handleIconMouseEnter("wishlist")}
-            onMouseLeave={handleIconMouseLeave}
-          >
-            <WishlistPopUp />
+            <div
+              className={`absolute top-full left-0 w-full transition-all duration-300 ${
+                visibleIconPopup === "account"
+                  ? "opacity-100 visible"
+                  : "opacity-0 invisible pointer-events-none"
+              }`}
+              onMouseEnter={() => handleIconMouseEnter("account")}
+              onMouseLeave={handleIconMouseLeave}
+            >
+              <AccountPopUp />
+            </div>
+            <div
+              className={`absolute top-full left-0 w-full transition-all duration-300 ${
+                visibleIconPopup === "wishlist"
+                  ? "opacity-100 visible"
+                  : "opacity-0 invisible pointer-events-none"
+              }`}
+              onMouseEnter={() => handleIconMouseEnter("wishlist")}
+              onMouseLeave={handleIconMouseLeave}
+            >
+              <WishlistPopUp />
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
